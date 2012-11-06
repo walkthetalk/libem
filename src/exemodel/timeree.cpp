6bf3854f54cdb8e132bc4bf53be5e5d908718fc6
justgaoyuan <gao_yuangy@126.com>
2012-11-06 13:55:16 +0000

    sdfasfd

---
+++ src/exemodel/timeree.cpp
@@ -0,0 +1,61 @@
+#include <sys/timerfd.h>
+
+#include <assert.h>
+#include <sys/epoll.h>
+
+#include "exemodel/timeree.hpp"
+#include "exemodel/poll_tools.hpp"
+
+namespace exemodel{
+timeree::timeree(struct timespec interval, bool reuse)
+: pollee(::timerfd_create(CLOCK_MONOTONIC,TFD_CLOEXEC),uint32_t(::EPOLLIN | ::EPOLLERR))
+, m_interval(interval)
+, m_reuse(reuse)
+{
+}
+
+timeree::~timeree()
+{
+}
+
+void timeree::start(void)
+{ 
+	//start timer
+	struct itimerspec tmpTime = {
+		m_interval,
+		m_interval,
+	};
+	int ret = timerfd_settime(_fd_(), 0, &tmpTime, NULL);
+	validate_ret(ret,"start timer error!\n");
+	
+}
+
+void timeree::stop(void)
+{
+	//stop timer
+	struct itimerspec tmpTime = {
+		 m_interval,
+		 {0,0},
+	};
+	int ret = timerfd_settime(_fd_(), 0, &tmpTime, NULL);
+	validate_ret(ret,"stop timer error!\n");
+}
+
+void timeree::dispose(poller & mgr, uint32_t evts)
+{	
+	if(evts & EPOLLERR){
+	      printf("timer error occurd fd %d\n",_fd_());
+	      return;
+	}
+	
+	if(evts & EPOLLIN){
+		timeree::args_t args = { mgr, *this, evts };
+		this->exe(args);
+	  
+		if(!m_reuse){
+			stop();
+		}
+	}
+}
+  
+}
