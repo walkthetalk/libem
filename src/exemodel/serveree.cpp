0a601e2c6d0246708261c685a50096f98ac4edac
Yi Qingliang <niqingliang2003@tom.com>
2013-03-21 17:04:09 +0800

    remove EPOLLET for server/client/connect

--- src/exemodel/serveree.cpp
+++ src/exemodel/serveree.cpp
@@ -11,7 +11,7 @@ namespace exemodel {
 
 serveree::serveree(uint16_t port)
 : pollee(::socket(AF_INET, ::SOCK_STREAM, 0),
-	(uint32_t)(::EPOLLIN | ::EPOLLET))
+	(uint32_t)(::EPOLLIN))
 , connectee::cb_t()
 , m_destroycb(std::bind(&serveree::destroy, this, std::placeholders::_1))
 , m_connectee(nullptr)
