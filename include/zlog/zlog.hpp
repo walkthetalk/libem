405ba9054742dd7565aef52925d900ba172316de
Yi Qingliang <niqingliang2003@tom.com>
2013-05-03 18:17:25 +0800

    add zlog

---
+++ include/zlog/zlog.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <syslog.h>
+#include <stdio.h>
+
+namespace zlog {
+
+template< typename... args_t >
+inline void zlog_base(int priority, const char *format, args_t... args)
+{
+#if 0
+	syslog(priority, format, args...);
+#else
+	printf("[%d] : ", priority);
+	printf(format, args...);
+	printf("\n");
+#endif
+}
+
+template< typename... args_t >
+inline void zlog_emerg(const char *format, args_t... args)
+{
+	zlog_base(LOG_EMERG, format, args...);
+}
+
+template< typename... args_t >
+inline void zlog_alert(const char *format, args_t... args)
+{
+	zlog_base(LOG_ALERT, format, args...);
+}
+
+template< typename... args_t >
+inline void zlog_crit(const char *format, args_t... args)
+{
+	zlog_base(LOG_CRIT, format, args...);
+}
+
+template< typename... args_t >
+inline void zlog_err(const char *format, args_t... args)
+{
+	zlog_base(LOG_ERR, format, args...);
+}
+
+template< typename... args_t >
+inline void zlog_warning(const char *format, args_t... args)
+{
+	zlog_base(LOG_WARNING, format, args...);
+}
+
+template< typename... args_t >
+inline void zlog_notice(const char *format, args_t... args)
+{
+	zlog_base(LOG_NOTICE, format, args...);
+}
+
+template< typename... args_t >
+inline void zlog_info(const char *format, args_t... args)
+{
+	zlog_base(LOG_INFO, format, args...);
+}
+
+template< typename... args_t >
+inline void zlog_debug(const char *format, args_t... args)
+{
+	zlog_base(LOG_DEBUG, format, args...);
+}
+
+} /* namespace zlog */
