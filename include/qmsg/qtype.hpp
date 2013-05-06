929545bfab2ae72bb25037f27fbff115b0cbb907
bingo <1@2.com>
2013-05-06 13:49:05 +0800

    add channel msg

---
+++ include/qmsg/qtype.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+
+typedef signed char CHAR;
+typedef unsigned char UCHAR;
+
+typedef signed short SHORT;
+typedef unsigned short USHORT;
+
+typedef signed long LONG;
+typedef unsigned long ULONG;
+
+typedef float FLOAT;
+
+typedef ULONG BOOL;
+
+typedef void VOID;
