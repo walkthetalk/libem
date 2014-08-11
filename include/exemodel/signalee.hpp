bde2728cee38bf2704b1787e8dc6d1e01fe39911
niql <niqingliangl2003@gmail.com>
2014-08-11 14:41:48 +0800

    add pollee: signalee
    Signed-off-by: niql <niqingliangl2003@gmail.com>

---
+++ include/exemodel/signalee.hpp
@@ -0,0 +1,42 @@
+#pragma once
+/**
+ * \file	exemodel/signalee.hpp
+ * \author	Yi Qingliang<niqingliang2003@gmail.com>
+ */
+
+#include <signal.h>
+
+#include <stdint.h>
+#include <vector>
+
+#include "exemodel/pollee.hpp"
+#include "exemodel/evt_cb.hpp"
+
+/**
+ *\class fifo
+ *\brief device pollee,used by \em poller
+ */
+namespace exemodel{
+class poller;
+
+class signalee
+: public pollee
+, public evt_cb< void > {
+public:
+	/**
+	 * \brief ctor of signalee
+	 * \param path	point to a pathname to open.
+	 */
+	explicit signalee(int signum);
+	virtual ~signalee();
+public:
+	/**
+	 * \brief used for disposing the event caught by the \em poller attached.
+	 */
+	virtual void dispose(poller &, uint32_t evts);
+private:
+	signalee(const signalee &rhs ) = delete;
+	signalee &operator = (signalee & rhs) = delete;
+};
+
+}
---
+++ src/exemodel/signalee.cpp
@@ -0,0 +1,48 @@
+#include <sys/signalfd.h>
+
+#include "zlog/zlog.hpp"
+
+#include "exemodel/poll_tools.hpp"
+#include "exemodel/signalee.hpp"
+
+namespace exemodel {
+
+static int gen_signalfd(int signum)
+{
+	sigset_t mask;
+	sigemptyset(&mask);
+	sigaddset(&mask, signum);
+
+	int ret = ::sigprocmask(SIG_BLOCK, &mask, NULL);
+	validate_ret(ret, "sigprocmask");
+
+	int sfd = ::signalfd(-1, &mask, 0);
+	validate_ret(sfd, "generate signal fd");
+
+	return sfd;
+}
+
+signalee::signalee(int signum)
+: pollee(gen_signalfd(signum),
+	 (uint32_t)(::EPOLLIN),
+	 "signalee")
+{
+}
+
+signalee::~signalee()
+{
+	/// \todo need deblock signum
+}
+
+void signalee::dispose(poller &, uint32_t evts)
+{
+	if ((evts & ::EPOLLIN)) {
+		struct signalfd_siginfo fdsi;
+		ssize_t ret = this->read(&fdsi, sizeof(fdsi));
+		if (ret == sizeof(fdsi)) {
+			this->exe();
+		}
+	}
+}
+
+}
