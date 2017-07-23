241f7529a549a4c803d400bf8857be78ba6c5998
Yi Qingliang <niqingliang2003@gmail.com>
2017-07-23 23:44:55 +0800

    update jmsg definition
    Signed-off-by: Yi Qingliang <niqingliang2003@gmail.com>

--- scripts/initdb.sql
+++ scripts/initdb.sql
@@ -2,7 +2,42 @@ CREATE TYPE EFTI_MODE		AS ENUM ('AUTO','FOLLOW','SM','DS','NZ','MM');
 CREATE TYPE EFUSION_MODE	AS ENUM ('AUTO','CALIBRATE','NORMAL','SPECIAL');
 CREATE TYPE EALIGN_MODE		AS ENUM ('AUTO','CLAD', 'CORE', 'FINE', 'MANUAL');
 CREATE TYPE ELOSSEST_MODE	AS ENUM ('OFF', 'FINE', 'CORE', 'CLAD');
-CREATE TYPE EFUNC_RESULT	AS ENUM ('SUCCESS', 'FAIL', 'INVALID');
+CREATE TYPE EFS_RESULT	AS ENUM (
+		'SUCCESS',
+		'MEDDIACY',
+		'UNKOWN_ERR',
+		'COVER_OPENNED',
+		'NO_FIBER',
+		'FIBER_DEFECT',
+		'FIBER_RECO',
+		'CROSS_OVER',
+		'OFF_CENTER',
+		'IMG_LUM',
+		'ABNORMAL_ARC',
+		'TENSE_TEST_FAIL',
+		'FIBER_BROKEN',
+		'QUIT_MIDWAY',
+		'PUSH_TIMEOUT',
+		'CALIBRATE_TIMEOUT',
+		'RESET_TIMEOUT',
+		'ARC_TIME_ZERO',
+		'ARC1_REVISE',
+		'ARC2_REVISE',
+		'FOCUS_X',
+		'FOCUS_Y',
+		'IMG_PROCESS',
+		'SYSTEM_ERR',
+		'FIBER_OFFSIDE',
+		'CMOS_EXP',
+		'LOSS_EST',
+		'ARC_POS',
+		'ARC_OVERFLOW');
+CREATE DOMAIN DT_VOLTAGE	AS DECIMAL(5,3);	-- unit: v
+CREATE DOMAIN DT_LENGTH		AS DECIMAL(8,3);	-- unit: um
+CREATE DOMAIN DT_TIME		AS INT4;		-- unit: ms
+CREATE DOMAIN DT_SPEED		AS INT4;		-- unit: um/s (nm/ms)
+CREATE DOMAIN DT_ANGLE		AS DECIMAL(5,2);	-- unit: degree
+CREATE DOMAIN DT_LOSS		AS DECIMAL(8,3);	-- unit: dB
 
 -- length     : xxxx.x um
 -- angle      :   xx.x degree
@@ -22,39 +57,39 @@ CREATE TABLE fs_param (
 	, ecf_redress	BOOL		NOT NULL	DEFAULT TRUE
 	, auto_mag	BOOL		NOT NULL	DEFAULT TRUE
 	, tense_test	BOOL		NOT NULL	DEFAULT FALSE
-	, tense_speed	INT4		NOT NULL	DEFAULT 200	-- unit: um/s
-	, tense_length	INT4		NOT NULL	DEFAULT 100	-- unit: um
-	, vangle_limit	DECIMAL(3,1)	NOT NULL	DEFAULT 2.0
-	, hangle_limit  DECIMAL(3,1)	NOT NULL	DEFAULT 1.0
-	, clr_mag	DECIMAL(5,3)	NOT NULL	DEFAULT 0.3
-	, clr_time	INT2		NOT NULL	DEFAULT 300
-	, clr_pos	DECIMAL(5,3)	NOT NULL	DEFAULT 100	-- unit: um
-	, position	DECIMAL(5,3)	NOT NULL	DEFAULT 0
-	, gap		DECIMAL(5,3)	NOT NULL	DEFAULT 8
-	, overlap	DECIMAL(5,3)	NOT NULL	DEFAULT 6
-	, pre_mag	DECIMAL(5,3)	NOT NULL	DEFAULT 0.3
-	, pre_time	INT2		NOT NULL	DEFAULT 150
-	, arc1_mag	DECIMAL(5,3)	NOT NULL	DEFAULT 0.82
-	, arc1_time	INT2		NOT NULL	DEFAULT 2200
-	, arc2_mag	DECIMAL(5,3)	NOT NULL	DEFAULT 0.4
-	, arc2_time	INT2		NOT NULL	DEFAULT 0
-	, arc2_on_time	INT2		NOT NULL	DEFAULT 180
-	, arc2_off_time	INT2		NOT NULL	DEFAULT 0
-	, arc_man_time	INT2		NOT NULL	DEFAULT 800
+	, tense_speed	DT_SPEED	NOT NULL	DEFAULT 200	-- unit: um/s
+	, tense_length	DT_LENGTH	NOT NULL	DEFAULT 100	-- unit: um
+	, vangle_limit	DT_ANGLE	NOT NULL	DEFAULT 2.0
+	, hangle_limit  DT_ANGLE	NOT NULL	DEFAULT 1.0
+	, clr_mag	DT_VOLTAGE	NOT NULL	DEFAULT 0.3	-- unit: volt
+	, clr_time	DT_TIME		NOT NULL	DEFAULT 300
+	, clr_pos	DT_LENGTH	NOT NULL	DEFAULT 100	-- unit: um
+	, position	DT_LENGTH	NOT NULL	DEFAULT 0
+	, gap		DT_LENGTH	NOT NULL	DEFAULT 8
+	, overlap	DT_LENGTH	NOT NULL	DEFAULT 6
+	, pre_mag	DT_VOLTAGE	NOT NULL	DEFAULT 0.3	-- unit: volt
+	, pre_time	DT_TIME		NOT NULL	DEFAULT 150
+	, arc1_mag	DT_VOLTAGE	NOT NULL	DEFAULT 0.82
+	, arc1_time	DT_TIME		NOT NULL	DEFAULT 2200
+	, arc2_mag	DT_VOLTAGE	NOT NULL	DEFAULT 0.4
+	, arc2_time	DT_TIME		NOT NULL	DEFAULT 0
+	, arc2_on_time	DT_TIME		NOT NULL	DEFAULT 180
+	, arc2_off_time	DT_TIME		NOT NULL	DEFAULT 0
+	, arc_man_time	DT_TIME		NOT NULL	DEFAULT 800
 	, taper_splice	BOOL		NOT NULL	DEFAULT FALSE
-	, taper_wait_time	INT2	NOT NULL	DEFAULT 400
-	, taper_length	DECIMAL(5,1)	NOT NULL	DEFAULT 10
-	, taper_speed	INT4		NOT NULL	DEFAULT 200	-- unit: um/s
+	, taper_wait_time	DT_TIME	NOT NULL	DEFAULT 400
+	, taper_length	DT_LENGTH	NOT NULL	DEFAULT 10
+	, taper_speed	DT_SPEED	NOT NULL	DEFAULT 200	-- unit: um/s
 	, loss_mode	ELOSSEST_MODE	NOT NULL	DEFAULT 'FINE'
-	, loss_limit	DECIMAL(5,3)	NOT NULL	DEFAULT 0.20
-	, loss_min	DECIMAL(5,3)	NOT NULL	DEFAULT 0.01
-	, lft_mfd	DECIMAL(5,3)	NOT NULL	DEFAULT 9.3
-	, rt_mfd	DECIMAL(5,3)	NOT NULL	DEFAULT 9.3
+	, loss_limit	DT_LOSS		NOT NULL	DEFAULT 0.20
+	, loss_min	DT_LOSS		NOT NULL	DEFAULT 0.01
+	, lft_mfd	DT_LENGTH	NOT NULL	DEFAULT 9.3
+	, rt_mfd	DT_LENGTH	NOT NULL	DEFAULT 9.3
 	, syn_bend_co	DECIMAL(5,2)	NOT NULL	DEFAULT 0.7
 	, opp_bend_co	DECIMAL(5,2)	NOT NULL	DEFAULT 0.3
 	, mfd_mis_co	DECIMAL(5,2)	NOT NULL	DEFAULT 0
-	, lft_push_speed	INT4	NOT NULL	DEFAULT 100	-- unit: um/s
-	, rt_push_speed		INT4	NOT NULL	DEFAULT 100	-- unit: um/s
+	, lft_push_speed	DT_SPEED	NOT NULL	DEFAULT 100	-- unit: um/s
+	, rt_push_speed		DT_SPEED	NOT NULL	DEFAULT 100	-- unit: um/s
 );
 
 INSERT INTO fs_param (
@@ -94,7 +129,7 @@ INSERT INTO fs_param_lib (
 	(11,	'Noraml NZ-NZ',	'NORMAL',	'NZ',	'FOLLOW',	'FINE',	FALSE,	FALSE,	TRUE,	FALSE,	FALSE,	2.0,	1.0,	0.30,	300,	0,	8,	6,	0.30,	150,	0.77,	2200,	0.40,	0,	180,	0,	800,	FALSE,	400,	10,	0.20,	'FINE',	0.2,	0.00,	9.3,	9.3,	0.3,	0.7,	0,	0.1,	0.1),
 	(12,	'Normal MM-MM',	'NORMAL',	'MM',	'FOLLOW',	'CLAD',	FALSE,	FALSE,	TRUE,	FALSE,	FALSE,	2.0,	1.0,	0.30,	300,	0,	8,	6,	0.30,	150,	0.70,	2200,	0.40,	0,	180,	0,	800,	FALSE,	400,	10,	0.20,	'CLAD',	0.2,	0.00,	50.0,	50.0,	0.3,	0.7,	0,	0.1,	0.1);
 
-CREATE TYPE EHEAT_MATERIAL		AS ENUM ('Standard', 'Micro-250', 'Micro-400', 'Micro-900', 'Connector');
+CREATE TYPE EHEAT_MATERIAL		AS ENUM ('Standard', 'Micro250', 'Micro400', 'Micro900', 'Connector');
 CREATE TYPE EHEAT_LENGTH		AS ENUM ('20mm', '40mm', '60mm');
 
 CREATE TABLE heat_param (
@@ -105,7 +140,7 @@ CREATE TABLE heat_param (
 	, auto_heat	BOOL		NOT NULL	DEFAULT TRUE
 	, heat_time	INT2		NOT NULL	DEFAULT 6
 	, heat_temp	INT2		NOT NULL	DEFAULT 150
-	, finish_temp	INT		NOT NULL	DEFAULT 140
+	, finish_temp	INT2		NOT NULL	DEFAULT 140
 	, fast_heat	BOOL		NOT NULL	DEFAULT FALSE
 	, temp_stay	INT2		NOT NULL	DEFAULT 60
 );
@@ -115,13 +150,13 @@ INSERT INTO heat_param (
 ) VALUES
 	(1,	'Standard 60mm',	'Standard',	'60mm',	FALSE,	6,	150,	140,	FALSE,	60),
 	(2,	'Standard 40mm',	'Standard',	'40mm',	FALSE,	6,	150,	140,	FALSE,	80),
-	(3,	'Micro-250-40mm',	'Micro-250',	'40mm',	FALSE,	35,	150,	80,	FALSE,	80),
-	(4,	'Micro-250-20mm',	'Micro-250',	'20mm',	FALSE,	35,	150,	80,	FALSE,	80),
-	(5,	'Micro-400-40mm',	'Micro-400',	'40mm',	FALSE,	35,	150,	80,	FALSE,	80),
-	(6,	'Micro-400-20mm',	'Micro-400',	'20mm',	FALSE,	35,	150,	80,	FALSE,	80),
-	(7,	'Micro-900-60mm',	'Micro-900',	'60mm',	FALSE,	35,	150,	80,	FALSE,	80),
-	(8,	'Micro-900-40mm',	'Micro-900',	'40mm',	FALSE,	35,	150,	80,	FALSE,	80),
-	(9,	'Micro-900-20mm',	'Micro-900',	'20mm',	FALSE,	35,	150,	80,	FALSE,	80);
+	(3,	'Micro-250-40mm',	'Micro250',	'40mm',	FALSE,	35,	150,	80,	FALSE,	80),
+	(4,	'Micro-250-20mm',	'Micro250',	'20mm',	FALSE,	35,	150,	80,	FALSE,	80),
+	(5,	'Micro-400-40mm',	'Micro400',	'40mm',	FALSE,	35,	150,	80,	FALSE,	80),
+	(6,	'Micro-400-20mm',	'Micro400',	'20mm',	FALSE,	35,	150,	80,	FALSE,	80),
+	(7,	'Micro-900-60mm',	'Micro900',	'60mm',	FALSE,	35,	150,	80,	FALSE,	80),
+	(8,	'Micro-900-40mm',	'Micro900',	'40mm',	FALSE,	35,	150,	80,	FALSE,	80),
+	(9,	'Micro-900-20mm',	'Micro900',	'20mm',	FALSE,	35,	150,	80,	FALSE,	80);
 
 --	heat_param lib
 CREATE TABLE heat_param_lib () INHERITS (heat_param);
@@ -131,28 +166,74 @@ INSERT INTO heat_param_lib (
 ) VALUES
 	(1,	'Standard 60mm',	'Standard',	'60mm',	FALSE,	6,	150,	140,	FALSE,	60),
 	(2,	'Standard 40mm',	'Standard',	'40mm',	FALSE,	6,	150,	140,	FALSE,	80),
-	(3,	'Micro-250-40mm',	'Micro-250',	'40mm',	FALSE,	35,	150,	80,	FALSE,	80),
-	(4,	'Micro-250-20mm',	'Micro-250',	'20mm',	FALSE,	35,	150,	80,	FALSE,	80),
-	(5,	'Micro-400-40mm',	'Micro-400',	'40mm',	FALSE,	35,	150,	80,	FALSE,	80),
-	(6,	'Micro-400-20mm',	'Micro-400',	'20mm',	FALSE,	35,	150,	80,	FALSE,	80),
-	(7,	'Micro-900-60mm',	'Micro-900',	'60mm',	FALSE,	35,	150,	80,	FALSE,	80),
-	(8,	'Micro-900-40mm',	'Micro-900',	'40mm',	FALSE,	35,	150,	80,	FALSE,	80),
-	(9,	'Micro-900-20mm',	'Micro-900',	'20mm',	FALSE,	35,	150,	80,	FALSE,	80),
+	(3,	'Micro-250-40mm',	'Micro250',	'40mm',	FALSE,	35,	150,	80,	FALSE,	80),
+	(4,	'Micro-250-20mm',	'Micro250',	'20mm',	FALSE,	35,	150,	80,	FALSE,	80),
+	(5,	'Micro-400-40mm',	'Micro400',	'40mm',	FALSE,	35,	150,	80,	FALSE,	80),
+	(6,	'Micro-400-20mm',	'Micro400',	'20mm',	FALSE,	35,	150,	80,	FALSE,	80),
+	(7,	'Micro-900-60mm',	'Micro900',	'60mm',	FALSE,	35,	150,	80,	FALSE,	80),
+	(8,	'Micro-900-40mm',	'Micro900',	'40mm',	FALSE,	35,	150,	80,	FALSE,	80),
+	(9,	'Micro-900-20mm',	'Micro900',	'20mm',	FALSE,	35,	150,	80,	FALSE,	80),
 	(10,	'Connector 40mm',	'Connector',	'40mm',	FALSE,	6,	150,	80,	FALSE,	80),
 	(11,	'Connector 20mm',	'Connector',	'20mm',	FALSE,	6,	150,	80,	FALSE,	80);
 
 CREATE TABLE fs_record (
 	  sn		SERIAL		NOT NULL	PRIMARY KEY
 	, exec_date	DATE		NOT NULL
-	, loss		DECIMAL(5,3)	NOT NULL
-	, langle	DECIMAL(3,1)	NOT NULL
-	, rangle	DECIMAL(3,1)	NOT NULL
-	, core_offset	DECIMAL(5,3)	NOT NULL
-	, clad_offset	DECIMAL(5,3)	NOT NULL
-	, endface_gap	DECIMAL(5,1)	NOT NULL
-	, vertex_intersect_angle	DECIMAL(3,1)	NOT NULL
-	, manual_arc_count	INT2	NOT NULL
-	, tense_result	EFUNC_RESULT	NOT NULL
+	, time_consume		INT4	NOT NULL
+	, code	EFS_RESULT	NOT NULL
+
+	, recinfo_lft_ft	EFTI_MODE	NOT NULL
+	, recinfo_lft_clad_dm	DT_LENGTH	NOT NULL
+	, recinfo_lft_core_dm	DT_LENGTH	NOT NULL
+
+	, recinfo_rt_ft		EFTI_MODE	NOT NULL
+	, recinfo_rt_clad_dm	DT_LENGTH	NOT NULL
+	, recinfo_rt_core_dm	DT_LENGTH	NOT NULL
+
+	, defect_yzl_dbmp	INT4		NOT NULL
+	, defect_yzl_hangle	DT_ANGLE	NOT NULL
+	, defect_yzl_vangle	DT_ANGLE	NOT NULL
+	, defect_yzl_clad_dm	DT_LENGTH	NOT NULL
+
+	, defect_yzr_dbmp	INT4		NOT NULL
+	, defect_yzr_hangle	DT_ANGLE	NOT NULL
+	, defect_yzr_vangle	DT_ANGLE	NOT NULL
+	, defect_yzr_clad_dm	DT_LENGTH	NOT NULL
+
+	, defect_xzl_dbmp	INT4		NOT NULL
+	, defect_xzl_hangle	DT_ANGLE	NOT NULL
+	, defect_xzl_vangle	DT_ANGLE	NOT NULL
+	, defect_xzl_clad_dm	DT_LENGTH	NOT NULL
+
+	, defect_xzr_dbmp	INT4		NOT NULL
+	, defect_xzr_hangle	DT_ANGLE	NOT NULL
+	, defect_xzr_vangle	DT_ANGLE	NOT NULL
+	, defect_xzr_clad_dm	DT_LENGTH	NOT NULL
+
+	, defect_yz_hangle	DT_ANGLE	NOT NULL
+	, defect_xz_hangle	DT_ANGLE	NOT NULL
+	, defect_lft_vangle	DT_ANGLE	NOT NULL
+	, defect_rt_vangle	DT_ANGLE	NOT NULL
+
+	, defect_yz_img		BYTEA
+	, defect_xz_img		BYTEA
+	, defect_yz_defect_img	BYTEA
+	, defect_xz_defect_img	BYTEA
+
+	, prestate_core_offset	DT_LENGTH	NOT NULL
+	, prestate_clad_offset	DT_LENGTH	NOT NULL
+	, prestate_endface_gap	DT_LENGTH	NOT NULL
+	, prestate_vertex_angle	DT_ANGLE	NOT NULL
+
+	, loss			DT_LOSS		NOT NULL
+
+	, manual_arc_count	INT2		NOT NULL
+
+	, tense_test_exed	BOOL		NOT NULL
+	, tense_test_pass	BOOL		NOT NULL
+
+	, xz_final_img		BYTEA
+	, yz_final_img		BYTEA
 ) INHERITS (fs_param);
 
 CREATE TABLE others (
