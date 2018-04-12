097d00c4df2a7566488f27d8f59e6ef7a9e2832a
Yi Qingliang <niqingliang2003@gmail.com>
2018-04-12 16:05:32 +0800

    timespec: restore 'timespec' for function name
    Signed-off-by: Yi Qingliang <niqingliang2003@gmail.com>

--- include/exemodel/timespec.hpp
+++ include/exemodel/timespec.hpp
@@ -13,14 +13,14 @@ typedef struct {
 } itimerspec_t;
 
 timespec_t ms_to_timespec(int ms);
-bool ts_equal(const timespec_t &a, const timespec_t &b);
+bool timespec_equal(const timespec_t &a, const timespec_t &b);
 /*
  * lhs < rhs:  return <0
  * lhs == rhs: return 0
  * lhs > rhs:  return >0
  */
-int ts_compare(const timespec_t &lhs, const timespec_t &rhs);
-timespec_t ts_add(const timespec_t lhs, const timespec_t rhs);
-timespec_t ts_sub(const timespec_t lhs, const timespec_t rhs);
+int timespec_compare(const timespec_t &lhs, const timespec_t &rhs);
+timespec_t timespec_add(const timespec_t lhs, const timespec_t rhs);
+timespec_t timespec_sub(const timespec_t lhs, const timespec_t rhs);
 
 }
